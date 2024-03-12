2726adb83b01923c03a71262dfc47a05e49bfbbb
Santiago <98421206+SantiagoHernandez20@users.noreply.github.com>
2024-03-12 12:35:21 -0500

    Update flask_app.sh

--- flask_app.sh
+++ flask_app.sh
@@ -1,43 +1,39 @@
 #!/bin/bash
 
-temp_dir=$(mktemp -d)
-cd $temp_dir
-# Crear una estructura de carpetas para el proyecto
-mkdir myflaskapp
-cd myflaskapp
-
-mkdir static
-mkdir templates
-
-# Copiar archivos al proyecto
+# Obtener la ruta del proyecto (directorio actual)
 proyecto_dir="$PWD"
-echo "la ruta del proyecto es $proyecto_dir"
 
-cp -r "$proyecto_dir/static" .
-cp -r "$proyecto_dir/templates" .
-cp "$proyecto_dir/flask_app.py" .
+# Crear una carpeta temporal
+temp_dir=$(mktemp -d)
 
-#cp ../flask_app.py .
-#cp -r ../templates/* templates/.
-#cp -r ../static/* static/.
+# Copiar archivos y carpetas del proyecto al directorio temporal
+cp -r "$proyecto_dir/static" "$temp_dir"
+cp -r "$proyecto_dir/templates" "$temp_dir"
+cp "$proyecto_dir/flask_app.py" "$temp_dir"
 
 # Crear el Dockerfile
-echo "FROM python" > Dockerfile
-echo "RUN pip install flask" >> Dockerfile
-echo "WORKDIR /home/myapp" >> Dockerfile
+cat <<EOF > "$temp_dir/Dockerfile"
+FROM python
+RUN pip install flask
+WORKDIR /home/myapp
+COPY ./static /home/myapp/static
+COPY ./templates /home/myapp/templates
+COPY flask_app.py /home/myapp/
+EXPOSE 5050
+CMD python3 /home/myapp/flask_app.py
+EOF
 
-echo "COPY ./static /home/myapp/static" >> Dockerfile
-echo "COPY ./templates /home/myapp/templates" >> Dockerfile
-echo "COPY flask_app.py /home/myapp/" >> Dockerfile
+# Entrar al directorio temporal
+cd "$temp_dir"
 
-echo "EXPOSE 5050" >> Dockerfile
-echo "CMD python3 /home/myapp/flask_app.py" >> Dockerfile
-
-# Construir la imagen y ejecutar el contenedor
+# Construir la imagen Docker
 docker build -t flaskweb .
+
+# Ejecutar el contenedor Docker
 docker run -t -d -p 5050:5050 -v /home/santiago/web_flask:/home/myapp --name flask_contenedor flaskweb
 
-
+# Mostrar información sobre el contenedor Docker
 docker ps -a
 
-rm -rf $temp_dir
+# Limpiar la carpeta temporal
+rm -rf "$temp_dir"
